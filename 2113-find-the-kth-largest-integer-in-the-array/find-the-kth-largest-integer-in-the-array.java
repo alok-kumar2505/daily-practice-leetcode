ab0ea26f035ba5386e10d7eea6270dffb767a2d9
Alok Kumar <148738056+alok-kumar2505@users.noreply.github.com>
2026-08-17 20:17:18 +0530

    Time: 22 ms (58.82%) | Memory: 51.5 MB (83.53%) - LeetSync

---
+++ 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String kthLargestNumber(String[] nums, int k) {
+        // Arrays.sort(nums,(a,b)->{
+        //     if(a.length()!=b.length()){
+        //         return a.length()-b.length();
+        //     }
+
+        //     return a.compareTo(b);
+        // });
+        // return nums[nums.length-k];
+        
+
+
+        //optimal
+
+        PriorityQueue <String> pq=new PriorityQueue<>((a,b)->{
+            if(a.length()!=b.length()){
+                return a.length()-b.length();
+            }
+
+            return a.compareTo(b);
+        });
+        for(String num:nums){
+            pq.add(num);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+
+        return pq.peek();
+    }
+}
+
